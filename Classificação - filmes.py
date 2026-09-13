9d23ab577d75788126a93cef810f7d4be092f2f6
vitorvasca-ux <vitor.vasca@gmail.comgit config --global user.name vitorvasca-uxgit config --global user.email vitor.vasca@gmail.com>
2026-09-13 00:46:29 -0300

    Adcionar exercício de classificação de filmes

---
+++ Classificação - filmes.py
@@ -0,0 +1,25 @@
+print("Bem-vindo à classificação de filmes")
+print("Você terá 5 filmes para classificar")
+
+classificacoes = []
+
+for contador in range(1, 6):
+
+    classificacao = input(f"Como você classifica o filme {contador}?: ")
+    classificacao = int(classificacao)
+
+    if classificacao == 0:
+        print("Sessão encerrada")
+        break
+
+    if classificacao < 1 or classificacao > 5:
+        print("Classificação inválida")
+    else:
+        classificacoes.append(classificacao)
+        print(f"Você classificou o filme {contador} com a nota {classificacao}")
+
+
+print("Lista de filmes classificados:")
+
+for i in range(1, len(classificacoes) + 1):
+    print(f"Filme {i}: Classificação {classificacoes[i - 1]}")
